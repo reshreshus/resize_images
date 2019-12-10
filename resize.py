ba444242ecbf2f027e392a503546b5128d98545a
reshreshus <reshreshus@gmail.com>
2019-12-10 03:01:43 +0300

    resize by width

--- resize.py
+++ resize.py
@@ -7,13 +7,15 @@ import os
 
 input_folder = 'input'
 output_folder = 'output'
-OUTPUT_SIZE = (256, 256)
+OUTPUT_SIZE_X = 256
 
 
 for image_name in os.listdir(input_folder):
     img = Image.open(os.path.join(input_folder, image_name))
     # I downsize the image with an ANTIALIAS filter (gives the highest quality)
-    img = img.resize(OUTPUT_SIZE, Image.ANTIALIAS)
+    width, height = img.size
+    output_size_y = int(height / (width / 256))
+    img = img.resize((OUTPUT_SIZE_X, output_size_y), Image.ANTIALIAS)
     img.save(os.path.join(output_folder, image_name), optimize=True,quality=95)
 
 
